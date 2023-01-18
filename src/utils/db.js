7ccd6f43b3e99b453cb382776f64b4846f67e83d
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-01-18 11:39:25 +0100

    submmit

---
+++ src/utils/db.js
@@ -0,0 +1,21 @@
+const mongoose = require ('mongoose');
+
+const DB_URL = 'mongodb+srv://root:root@cluster0.g6xerbs.mongodb.net/students-api';
+
+const connectDB = async () => {
+    try {
+        mongoose.set("strictQuery", true)
+        const db = await mongoose.connect(DB_URL);
+        const { name, host, port } = db.connection;
+        console.log(db.connection);
+        console.log('[Server Error] Conectado con éxito a: &{name} en host ${host} en puerto ${port}');
+    }
+    catch(error) {
+        console.log('[Server Error] Conectando a la base de datos', error);
+    }
+}; 
+
+module.exports = {
+    connectDB
+}
+
